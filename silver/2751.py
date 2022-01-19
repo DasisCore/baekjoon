627c147c402a07e73048269eb4d317981c2d947f
Heojaeyoung <gj30347896@gmail.com>
2022-01-20 01:58:29 +0900

    2022-01-20

---
+++ silver/2751.py
@@ -0,0 +1,27 @@
+# 2751. 수 정렬하기 2
+# 문제
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 
+# 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+
+# 출력
+# 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+test = int(input())
+
+li = []
+for i in range(test):
+    a = int(input())
+    li.append(a)
+
+li.sort()
+for i in range(len(li)):
+    print(li[i])   #-----> 시간 초과 --> pypy로 해결 
+    #                     pypy없이 해결할 방법 찾아볼 것.
+
+# 내장함수 list.sort()의 정렬 방법. => 시간복잡도 O(NlogN)
+
+# 교환 방식(Selection, Bubble, Quick), 삽입 방식(Insertion, Shell), 
+# 병합 방식(2-way 병합, n-way 병합), 분배 방식(Radix), 선택 방식(Heap, Tree)
