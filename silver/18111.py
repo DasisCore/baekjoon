4ddbb98e20d0ddb36a4f77d33b66fe0babf96af3
Heojaeyoung <gj30347896@gmail.com>
2022-02-01 19:11:43 +0900

    solved.ac class2 clear!

--- silver/18111.py
+++ silver/18111.py
@@ -11,54 +11,28 @@ for i in range(n): # 입력된 땅의 높낮이를 2차원 리스트로 형성
     space.append(coo)
     s += sum(coo)
 
-#만들어야할 땅의 높이.
-aver = round(s/(n*m))
-
-block = s - aver*(n*m) + b
-while True:
-    if block < 0:
-        aver -= 1
+# 여기서의 i값은 땅의 높이임.
+answer = [] #완전탐색, 각 높이에 대한 결과값이 저장될 곳
+for high in range(0, 257): #전체 높이
+    time = 0 # 각 높이의 회차 마다 초기화 시켜줌
+    block = b
+    for i in space:
+        for j in i:
+            if j == high:
+                pass
+            elif j < high: #블록 쌓기
+                di = abs(j - high)
+                time += di * 1
+                block -= di * 1
+            else: # 땅 파고 인벤토리
+                di = abs(j - high) # 땅을 파고 인벤토리에 넣는 시간
+                time += di*2 
+                block += di * 1
+    if block < 0: #블록이 모자랄 경우 break
         break
     else:
-        break
+        answer.append((time, high))
 
-
-time = 0
-answer = []
-for i in space:
-    for j in i:
-        if j == aver:
-            pass
-        elif j < aver: # 블록을 쌓는 시간
-            di = abs(j - aver)
-            time += di * 1
-            b -= di * 1
-        elif j > aver:
-            di = abs(j - aver) # 땅을 파고 인벤토리에 넣는 시간
-            time += di*2 
-            b += di * 1
-else:
-    answer.append((time, aver))
-
-time = 0
-for i in space:
-    for j in i:
-        if j == aver+1:
-            pass
-        elif j < aver+1: # 블록을 쌓는 시간
-            di = abs(j - aver+1)
-            time += di * 1
-            b -= di * 1
-        elif j > aver+1:
-            di = abs(j - aver+1) # 땅을 파고 인벤토리에 넣는 시간
-            time += di*2 
-            b += di * 1
-else:
-    answer.append((time, aver+1))
-
-answer.sort(key= lambda x : x[1], reverse = True)
-if answer[0][0] == answer[1][0]:
-    print(*answer[0])
-else:
-    print(*answer[1])
-
+answer.sort(key= lambda x : (x[0], -x[1])) 
+#  x[0]을 기준으로 정렬, 이후 값이 같을 경우 x[1]의 내림차순
+print(*answer[0])
