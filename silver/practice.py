28b049c5b1247bbb44b1a1d1f99f90db5a13a28c
Heojaeyoung <gj30347896@gmail.com>
2022-02-09 22:22:35 +0900

    22-02-09

--- silver/practice.py
+++ silver/practice.py
@@ -1,36 +1,25 @@
 # 이곳은 문제를 풀며 자유롭게 코드를 짜볼 수 있는 공간입니다.
-# def prime_list(n):
-#     # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
-#     sieve = [True] * (n+1)
 
-#     for i in range(2, n + 1):
-#         if i * i > n:
-#             break
-#         if not sieve[i]:
-#             continue
-#     for j in range(i * i, n + 1, i):
-#         sieve[i] = False
 
-#     # 소수 목록 산출
-#     return [i for i in range(2, n) if sieve[i] == True]
+li = list(map(int, input()))
+li.sort()
 
-# print(prime_list(16))
+answer = []
+for i in range(0, len(li)):
+    if len(answer) > 2 and li[i] == answer[-1] and li[i] == answer[-2]:
+        answer.pop()
+        answer.pop()
+    else:
+        answer.append(li[i])
 
-def prime_list(n):
-    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
-    sieve = [True] * (n+1)
-
-    i = 2
-    while i * i <= n:
-        if not sieve[i]:
-            continue
-        
-        for i in range(i * i, n + 1, i):
-            sieve[i] = False
-        
-        i += 1
-
-    # 소수 목록 산출
-    return [i for i in range(2, n) if sieve[i] == True]
-
-print(prime_list(16))
+if len(answer) > 3:
+    if answer[3] + 1 == answer[4] and answer[4] + 1 == answer[5]:
+        print("babygin!")
+    else:
+        print("No babygin!")
+elif answer[0] == answer[1] == answer[2]:
+    print("babygin!")
+elif answer[0] + 1 == answer[1] and answer[1] + 1 == answer[2]:
+    print("babygin!")
+else:
+    print("No babygin")
